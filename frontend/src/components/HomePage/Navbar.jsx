219a6e9156ed40cd6b5cdf32f7a9dfb8e1565f46
CodeCrusaderWiz <gurpreet0816@gmail.com>
2025-05-08 20:36:16 +0530

    latest

--- frontend/src/components/HomePage/Navbar.jsx
+++ frontend/src/components/HomePage/Navbar.jsx
@@ -192,7 +192,7 @@ const Navbar = () => {
                         {searchResults.map((book) => (
                           <Link
                             key={book.id}
-                            to={`/ebook-detail/${book.id}`}
+                            to={`/ebook-detail/`}
                             onClick={closeSearchResults}
                             className="block px-3 py-3 hover:bg-gray-50"
                           >
